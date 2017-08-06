16ffaa494b90c8396fa91c577d255303f7ee465a
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-08-06 21:32:06 +0300

    removed sample filter from author

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -3,169 +3,170 @@ import { Link } from 'react-router-dom'
 import { PropTypes } from 'prop-types'
 import moment from 'moment'
 
-import { Table, Badge, Tooltip, Icon } from 'antd'
+import { Table, Badge, Tooltip, Icon, Button, Input } from 'antd'
 const { Column } = Table
 
 import './Topics.scss'
 
-const Topics = props => {
-  const { topics, curriculum, handleTableChange } = props
-  const { data, count, loading, query } = topics
-  const { tab, sub, page, columnKey, order, filters } = query
-  const { name, type } = curriculum.data // TODO check if curriculum has SE or KU
-  const currentPage = page ? parseInt(page) : 1
+class Topics extends React.Component {
+  constructor(props) {
+    super(props)
 
-  const supervisors = {
-    data: [
-      { profile: { firstName: 'Romil', lastName: 'Robtsenkov', _id: 1 } },
-      { profile: { firstName: 'Ants', lastName: 'Tamm', _id: 2 } }
-    ]
-  }
-  const pagination = {
-    pageSize: 20,
-    total: count[sub] || 0,
-    current: currentPage,
-    size: ''
+    //this state
   }
 
-  return (
-    <div>
-      <br />
-      <Table
-        size="small"
-        loading={{ spinning: loading, delay: 200 }}
-        onChange={handleTableChange}
-        pagination={
-          // fix for pagination shift when changing tabs
-          tab === 'topics' ? pagination : false
-        }
-        rowKey={r => r._id}
-        dataSource={data}
-        rowClassName={record => (record.description ? '' : 'no-desc')}
-        expandedRowRender={sub === 'available' ? renderExpandedRow : false}
-        bordered
-      >
-        <Column
-          title="Title"
-          dataIndex="title"
-          key="title"
-          render={renderTitle}
-          sorter={true}
-          sortOrder={columnKey === 'title' && order}
-        />
+  render() {
+    const { topics, curriculum, handleTableChange } = this.props
+    let { data, count, loading, query } = topics
+    const { tab, sub, page, columnKey, order, filters } = query
+    const { name, type } = curriculum.data // TODO check if curriculum has SE or KU
+    const currentPage = page ? parseInt(page) : 1
 
-        {sub === 'available' &&
-          <Column
-            className="align-col-center"
-            filters={[{ text: 'Sobib teistele õppekavadele', value: 'ÕK' }]}
-            filterMultiple={false}
-            title={
-              <Tooltip placement="top" title={'Sobib teistele õppekavadele'}>
-                {'ÕK'}
-              </Tooltip>
-            }
-            dataIndex="curriculums"
-            key="curriculums"
-            filteredValue={
-              filters && filters.curriculums ? filters.curriculums : null
-            }
-            render={renderCurriculums}
-          />}
+    const pagination = {
+      pageSize: 20,
+      total: count[sub] || 0,
+      current: currentPage,
+      size: ''
+    }
 
-        {name === 'Informaatika' &&
-          type === 'BA' &&
-          <Column
-            className="align-col-center"
-            filterMultiple={false}
-            filters={[{ text: 'Sobib seminaritööks', value: 'SE' }]}
-            sorter={true}
-            sortOrder={columnKey === 'types' && order}
-            title={
-              <Tooltip
-                placement="top"
-                title={
-                  sub === 'available' ? 'Sobib seminaritööks' : 'Seminaritöö'
-                }
-              >
-                {'SE'}
-              </Tooltip>
-            }
-            dataIndex="types"
-            key="types"
-            filteredValue={filters && filters.types ? filters.types : null}
-            render={renderType}
-          />}
-        {(sub === 'registered' || sub === 'defended') &&
-          <Column
-            title="Author"
-            filters={[{ text: 'ÕK', value: 'ÕK' }]}
-            filteredValue={filters && filters.author ? filters.author : null}
-            dataIndex="author"
-            key="author"
-            sortOrder={columnKey === 'author' && order}
-            render={renderAuthor}
-            sorter={true}
-          />}
-
-        <Column
-          title="Supervisor(s)"
-          dataIndex="supervisors"
-          key="supervisors"
-          filteredValue={
-            filters && filters.supervisors ? filters.supervisors : null
+    return (
+      <div>
+        <br />
+        <Table
+          size="small"
+          loading={{ spinning: loading, delay: 200 }}
+          onChange={handleTableChange}
+          pagination={
+            // fix for pagination shift when changing tabs
+            tab === 'topics' ? pagination : false
           }
-          filters={supervisors.data.map(s => {
-            return {
-              text: s.profile.firstName + ' ' + s.profile.lastName,
-              value: s.profile.firstName + ' ' + s.profile.lastName
+          rowKey={r => r._id}
+          dataSource={data}
+          rowClassName={record => (record.description ? '' : 'no-desc')}
+          expandedRowRender={sub === 'available' ? renderExpandedRow : false}
+          bordered
+        >
+          <Column
+            title="Title"
+            dataIndex="title"
+            key="title"
+            render={renderTitle}
+            sorter={true}
+            sortOrder={columnKey === 'title' && order}
+          />
+
+          {sub === 'available' &&
+            <Column
+              className="align-col-center"
+              filters={[{ text: 'Sobib teistele õppekavadele', value: 'ÕK' }]}
+              filterMultiple={false}
+              title={
+                <Tooltip placement="top" title={'Sobib teistele õppekavadele'}>
+                  {'ÕK'}
+                </Tooltip>
+              }
+              dataIndex="curriculums"
+              key="curriculums"
+              filteredValue={
+                filters && filters.curriculums ? filters.curriculums : null
+              }
+              render={renderCurriculums}
+            />}
+
+          {name === 'Informaatika' &&
+            type === 'BA' &&
+            <Column
+              className="align-col-center"
+              filterMultiple={false}
+              // TODO other text value for other tabs
+              filters={[
+                {
+                  text:
+                    sub === 'available' ? 'Sobib seminaritööks' : 'Seminaritöö',
+                  value: 'SE'
+                }
+              ]}
+              sorter={true}
+              sortOrder={columnKey === 'types' && order}
+              title={
+                <Tooltip
+                  placement="top"
+                  title={
+                    sub === 'available' ? 'Sobib seminaritööks' : 'Seminaritöö'
+                  }
+                >
+                  {'SE'}
+                </Tooltip>
+              }
+              dataIndex="types"
+              key="types"
+              filteredValue={filters && filters.types ? filters.types : null}
+              render={renderType}
+            />}
+          {(sub === 'registered' || sub === 'defended') &&
+            <Column
+              title="Author"
+              dataIndex="author"
+              key="author"
+              sortOrder={columnKey === 'author' && order}
+              render={renderAuthor}
+              sorter={true}
+            />}
+
+          <Column
+            title="Supervisor(s)"
+            dataIndex="supervisors"
+            key="supervisors"
+            filteredValue={
+              filters && filters.supervisors ? filters.supervisors : null
             }
-          })}
-          render={renderSupervisors}
-        />
+            render={renderSupervisors}
+          />
 
-        {sub === 'registered' &&
-          <Column
-            title="Registered"
-            dataIndex="registered"
-            key="registered"
-            className="align-col-right"
-            render={renderDate}
-            sorter={true}
-            sortOrder={columnKey === 'registered' && order}
-          />}
+          {sub === 'registered' &&
+            <Column
+              title="Registered"
+              dataIndex="registered"
+              key="registered"
+              className="align-col-right"
+              render={renderDate}
+              sorter={true}
+              sortOrder={columnKey === 'registered' && order}
+            />}
 
-        {sub === 'defended' &&
-          <Column
-            title="Defended"
-            className="align-col-right"
-            dataIndex="defended"
-            key="defended"
-            render={renderDate}
-            sorter={true}
-            sortOrder={columnKey === 'defended' && order}
-          />}
-        {sub === 'defended' &&
-          <Column
-            title=""
-            className="align-col-right"
-            dataIndex="file"
-            key="file"
-            render={renderFile}
-            sortOrder={columnKey === 'file' && order}
-          />}
-        {sub === 'available' &&
-          <Column
-            title="Added"
-            className="align-col-right"
-            dataIndex="accepted"
-            key="accepted"
-            render={renderDate}
-            sorter={true}
-            sortOrder={columnKey === 'accepted' && order}
-          />}
-      </Table>
-    </div>
-  )
+          {sub === 'defended' &&
+            <Column
+              title="Defended"
+              className="align-col-right"
+              dataIndex="defended"
+              key="defended"
+              render={renderDate}
+              sorter={true}
+              sortOrder={columnKey === 'defended' && order}
+            />}
+          {sub === 'defended' &&
+            <Column
+              title=""
+              className="align-col-right"
+              dataIndex="file"
+              key="file"
+              render={renderFile}
+              sortOrder={columnKey === 'file' && order}
+            />}
+          {sub === 'available' &&
+            <Column
+              title="Added"
+              className="align-col-right"
+              dataIndex="accepted"
+              key="accepted"
+              render={renderDate}
+              sorter={true}
+              sortOrder={columnKey === 'accepted' && order}
+            />}
+        </Table>
+      </div>
+    )
+  }
 }
 
 const renderFile = file => {
