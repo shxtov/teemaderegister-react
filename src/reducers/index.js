06e0454b3f9a816bebbd6b118338b54992b498f5
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-08-13 20:15:07 +0300

    removed old reducers topics, supervisors

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,8 +4,6 @@ import auth from './auth'
 import curriculum from '../components/Curriculum/CurriculumReducers'
 import home from '../components/Home/HomeReducers'
 import login from '../components/Login/LoginReducers'
-import supervisors from '../components/Supervisors/SupervisorsReducers'
-import topics from '../components/Topics/TopicsReducers'
 import supervisor from '../components/Supervisor/SupervisorReducers'
 import tableContent from '../components/TableContent/TableContentReducers'
 
@@ -14,8 +12,6 @@ const rootReducer = combineReducers({
   curriculum,
   home,
   login,
-  supervisors,
-  topics,
   supervisor,
   tableContent
 })
